60c2aadad970e39c8648761f9ccb5237a9d5469c
magnuscadeaux <sales@magnuscadeaux.com>
2026-07-15 18:29:10 +0530

    Update registration-view.tsx
    Changed Routes.arena to Routes.challenge

--- features/registration/components/registration-view.tsx
+++ features/registration/components/registration-view.tsx
@@ -105,7 +105,7 @@ export function RegistrationView() {
                 email: wizard.draft.email,
                 phone: wizard.draft.phone,
               });
-              router.push(ROUTES.arena);
+              router.push(ROUTES.challenge);
             }}
           />
         ) : null}
